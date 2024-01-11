f224222b46607f6421256449600c0542a5c00776
abdullahbuhayia <buhayia12@gmail.com>
2024-01-11 23:36:54 +0600

    adding Sum of an arry element

--- arraySum.js
+++ arraySum.js
@@ -1,7 +1,20 @@
-let num = [9, 8, 3, 6, 4, 1, 5, 7, 2, 10];
-let arraySum = num[0];
-for (i = 0; i < num.length; i++) {
-    let arrayElement = num[i];
-    arraySum = arrayElement + arraySum;
+// let num = [9, 8, 3, 6, 4, 1, 5, 7, 2, 10];
+// let arraySum = num[0];
+// for (i = 0; i < num.length; i++) {
+//     let arrayElement = num[i];
+//     arraySum = arrayElement + arraySum;
+// }
+// console.log(arraySum);
+
+function getArray(num) {
+    let arraySum = num[0];
+    for (i = 0; i < num.length; i++) {
+        let arrayElement = num[i];
+        arraySum = arrayElement + arraySum;
+    }
+    return arraySum;
+
 }
-console.log(arraySum);
+let num = [9, 8, 3, 6, 4, 1, 5, 7, 2, 10];
+let result = getArray(num)
+console.log(result);
